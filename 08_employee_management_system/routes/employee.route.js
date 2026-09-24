77238bc0cd7b21ac6e95915c11f3e83e5e3e2559
vraj-111 <vrajd9313@gmail.com>
2026-09-24 11:30:58 +0530

    Delete 08_employee_management_system directory

--- 08_employee_management_system/routes/employee.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import employeeController from "../controller/employee.controller.js";
-
-const router = express.Router();
-
-router.post("/add", employeeController.add);
-
-export default router;
